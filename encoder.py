35176b795a9e0d557505cedff5b8991f411ef40c
spug-school <tuukka.lahdenpohja@metropolia.fi>
2025-05-05 21:01:02 +0300

    Encoder can now navigate menu

--- encoder.py
+++ encoder.py
@@ -4,15 +4,24 @@ import utime
 
 
 class Encoder:
-    def __init__(self, c):
-        self.c = Pin(c, Pin.IN, Pin.PULL_UP)
+    def __init__(self, rot_a, push_c):
+        self.a = Pin(rot_a, Pin.IN)
+        self.c = Pin(push_c, Pin.IN, Pin.PULL_UP)
         self.fifo = Fifo(30)
+        self.a.irq(self.handler, Pin.IRQ_RISING, hard = True)
         self.c.irq(self.button_handler, Pin.IRQ_RISING, hard = True)
-        self.INTERRUPTION_DELAY = 150
+        self.INTERRUPTION_DELAY = 250
         self.time_interrupted = 0
     
     def button_handler(self, pin):
         current_time = utime.ticks_ms()
-        if (utime.ticks_diff(current_time, self.time_interrupted) > self.INTERRUPTION_DELAY):    
+        if (utime.ticks_diff(current_time, self.time_interrupted) > self.INTERRUPTION_DELAY):
             self.fifo.put(0)
         self.time_interrupted = current_time
+    
+    def handler(self, pin):
+        current_time = utime.ticks_ms()
+        if (utime.ticks_diff(current_time, self.time_interrupted) > self.INTERRUPTION_DELAY):
+            self.time_interrupted = current_time
+            if self.a():
+                self.fifo.put(1)
